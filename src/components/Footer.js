4abbaf33d940759e240d0fd9575b03cc8e50d6f9
Krithe Kishan <krithe57@gmail.com>
2023-05-31 23:08:08 +0530

    Third commit
    Fixing footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -6,9 +7,9 @@ const Footer = () => {
       <div className="w-full mx-auto max-w-screen-xl other:flex other:justify-evenly p-4 md:flex md:items-center md:justify-between">
         <span className=" text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="#" className="hover:underline">
+          <span >
             MovieMate
-          </a>
+          </span>
           . All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3  font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
